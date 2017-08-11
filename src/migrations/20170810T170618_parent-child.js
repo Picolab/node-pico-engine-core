14808fec649db029069c4c9b4e896bec0356ee9e
Matthew Wright <matthew@smallhelm.com>
2017-08-10 22:16:42 -0500

    migration for parent relationships in ent:* to be managed by core

---
+++ src/migrations/20170810T170618_parent-child.js
@@ -0,0 +1,51 @@
+var dbRange = require("../dbRange");
+
+module.exports = {
+    up: function(ldb, callback){
+        // /entvars/:pico_id/io.picolabs.pico/parent
+        // /entvars/:pico_id/io.picolabs.pico/children
+
+        var to_batch = [];
+
+        dbRange(ldb, {
+            prefix: ["entvars"],
+        }, function(data){
+
+            var pico_id = data.key[1];
+
+            if(data.key[2] !== "io.picolabs.pico"){
+                return;
+            }
+            if(data.key[3] === "parent"){
+
+                var parent_id = data.value.id;
+
+                to_batch.push({
+                    type: "put",
+                    key: ["pico", pico_id],
+                    value: {
+                        id: pico_id,
+                        parent_id: parent_id,
+                    },
+                });
+                to_batch.push({
+                    type: "put",
+                    key: ["pico-children", parent_id, pico_id],
+                    value: true,
+                });
+
+                to_batch.push({type: "del", key: data.key});
+
+            }else if(data.key[3] === "children"){
+
+                to_batch.push({type: "del", key: data.key});
+
+            }
+
+        }, function(err){
+            if(err) return callback(err);
+
+            ldb.batch(to_batch, callback);
+        });
+    },
+};
